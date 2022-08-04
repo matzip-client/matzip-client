0999c9df79f13b52d3812687bfdc26543faa9cd6
장현진 <haegu@janghyeonjin-ui-MacBookPro.local>
2022-08-04 16:57:00 +0900

    redirect to Login page

--- src/MainComponent/MainComponent.jsx
+++ src/MainComponent/MainComponent.jsx
@@ -1,10 +1,24 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 function MainComponent({ isAuthorized, userInfo }) {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isAuthorized == null) {
+      navigate('/login');
+    }
+  }, [isAuthorized, navigate]);
   return (
     <div>
       <h1>Hi !</h1>
-      <dd>{userInfo.id}</dd>
-      <dd>{userInfo.password}</dd>
-      <dd>{isAuthorized}</dd>
+      {isAuthorized == null ? (
+        <h1>로그인 페이지로 이동합니다.</h1>
+      ) : (
+        <div>
+          <dd>{userInfo.id}</dd>
+          <dd>isAuthorized</dd>
+        </div>
+      )}
     </div>
   );
 }
