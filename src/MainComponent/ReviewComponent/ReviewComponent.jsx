d885e92831407badb4b059335f8c4df46a725d9c
yen001004 <yenn001004@gmail.com>
2022-08-24 23:51:41 +0900

    좋아요 버튼 추가

--- src/MainComponent/ReviewComponent/ReviewComponent.jsx
+++ src/MainComponent/ReviewComponent/ReviewComponent.jsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import ReviewStyles from './ReviewComponent.module.css';
 
 function ReviewComponent({ reviewObj }) {
+  const [liked, setLiked] = useState(false);
+  const toggleLiked = () => setLiked((prev) => !prev);
+
   return (
     <>
       <div className={ReviewStyles.card}>
@@ -8,6 +12,11 @@ function ReviewComponent({ reviewObj }) {
         <h4>{reviewObj.rating}점</h4>
         <h4>{reviewObj.content}</h4>
         <img src={reviewObj.image_urls} style={{ width: '200px', height: '200px' }} />
+        <br />
+        <i
+          className={liked ? 'fas fa-solid fa-heart' : 'fas fa-regular fa-heart'}
+          onClick={toggleLiked}
+        />
       </div>
     </>
   );
