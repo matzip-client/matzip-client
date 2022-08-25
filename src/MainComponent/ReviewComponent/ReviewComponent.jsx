dc9d0af2bc4b67c14f2e279ffd003b837d3cf9f9
yen001004 <yenn001004@gmail.com>
2022-08-25 22:10:43 +0900

    Delete button 추가

--- src/MainComponent/ReviewComponent/ReviewComponent.jsx
+++ src/MainComponent/ReviewComponent/ReviewComponent.jsx
@@ -1,12 +1,16 @@
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
 import ReviewStyles from './ReviewComponent.module.css';
 import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons';
 function ReviewComponent({ reviewObj }) {
   const [liked, setLiked] = useState(false);
-  const toggleLiked = () => setLiked((prev) => !prev);
-
+  const toggleLiked = () => {
+    setLiked((prev) => !prev);
+  };
+  const onDeleteClick = () => {
+    confirm('정말로 삭제하시겠습니까?');
+  };
   return (
     <>
       <div className={ReviewStyles.card}>
@@ -19,6 +23,12 @@ function ReviewComponent({ reviewObj }) {
           Like&nbsp;
           <FontAwesomeIcon icon={liked ? faHeart : faHeartRegular} />
         </button>
+        {reviewObj.is_deletable ? (
+          <button className={ReviewStyles.delete} onClick={onDeleteClick}>
+            Delete&nbsp;
+            <FontAwesomeIcon icon={faTrashCan} />
+          </button>
+        ) : null}
       </div>
     </>
   );
